3c7357c3f20b7801ef39c798c9690acb199fd536
dontbesad <1399530460@qq.com>
2018-01-07 21:46:47 +0800

    用户注册完善

--- api/application/libraries/User/Action_Register.php
+++ api/application/libraries/User/Action_Register.php
@@ -4,31 +4,38 @@ class Action_Register {
     const KEY_LIST = [
         'username' => [
             'must' => 1,
-            'preg' => '/^\w+$/'
+            'preg' => '/^\w+$/',
+            'hint' => '用户名只能包含数字英文下划线'
         ],
         'nickname' => [
             'must' => 1,
-            'preg' => '/^[(\x{4e00}-\x{9fa5})\w]+$/u'
+            'preg' => '/^[(\x{4e00}-\x{9fa5})\w]+$/u',
+            'hint' => '昵称只能包含中文数字英文下划线'
         ],
         'password' => [
             'must' => 1,
-            'preg' => '/^\w+$/'
+            'preg' => '/^\w+$/',
+            'hint' => '密码只能包含数字英文下划线'
         ],
         'password_again' => [
             'must' => 1,
-            'preg' => '/^\w+$/'
+            'preg' => '/^\w+$/',
+            'hint' => '重复输入的密码只能包含数字英文下划线'
         ],
         'school' => [
             'must' => 0,
-            'preg' => '/^\w+$/'
+            'preg' => '/^[(\x{4e00}-\x{9fa5})\w]+$/u',
+            'hint' => '学校只能包含中文数字英文下划线'
         ],
         'email' => [
             'must' => 0,
-            'preg' => '/^\w+@\w+\.\w+$/'
+            'preg' => '/^\w+@\w+\.\w+$/',
+            'hint' => '邮箱格式不正确'
         ],
         'description' => [
             'must' => 0,
-            'preg' => '/^[\w\s]+$/'
+            'preg' => '/^[(\x{4e00}-\x{9fa5})\w]+$/u',
+            'hint' => '描述只能包含中文数字英文下划线'
         ],
     ];
 
@@ -36,7 +43,7 @@ class Action_Register {
         $data = [];
         foreach (self::KEY_LIST as $key => $rule) {
             if (!empty($post_data[$key]) && !preg_match($rule['preg'], $post_data[$key])) {
-                throw new Exception($key.'格式不正确', 403);
+                throw new Exception($rule['hint'], 403);
             } else if ($rule['must'] && empty($post_data[$key])) {
                 throw new Exception($key.'不能为空', 403);
             } else if (!empty($post_data[$key])) {
